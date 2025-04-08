873013a2171bcbdbcb80ca438e1128f67fc3f97c
omrajkamat68 <ob2614@srmist.edu.in>
2025-04-08 10:43:12 +0530

    Product category and Product Details page added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -57,6 +57,27 @@ export const AppContextProvider = ({ children }) => {
     setCartItems(cartData);
   };
 
+  // Get Cart Item Count
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const item in cartItems) {
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  };
+
+  // Get Cart Total Amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      if (cartItems[items] > 0) {
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  };
+
   useEffect(() => {
     fetchProducts();
   }, []);
@@ -77,6 +98,8 @@ export const AppContextProvider = ({ children }) => {
     cartItems,
     searchQuery,
     setSearchQuery,
+    getCartCount,
+    getCartAmount,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
