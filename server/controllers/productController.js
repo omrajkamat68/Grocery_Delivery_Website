b0f61f2ff85f161e0096df4fe5f76bcbb67e933d
omrajkamat68 <ob2614@srmist.edu.in>
2025-04-13 09:46:44 +0530

    Controller functions for getting product list, getting product by id, changing stock, and updating cart added

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,41 +3,60 @@ import Product from "../models/Product.js";
 
 // Add Product
 export const addProduct = async (req, res) => {
-    try {
-        let productData = JSON.parse(req.body.productData)
+  try {
+    let productData = JSON.parse(req.body.productData);
 
-        const images = req.files
+    const images = req.files;
 
-        let imagesUrl = await Promise.all(
-            images.map(async (item)=>{
-                let result = await connectCloudinary.uploader.upload(item.path,
-                    {resource_type: 'image'}
-                )
-                return result.secure_url
-            })
-        )
+    let imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        let result = await connectCloudinary.uploader.upload(item.path, {
+          resource_type: "image",
+        });
+        return result.secure_url;
+      })
+    );
 
-        await Product.create({...productData, image:imagesUrl})
+    await Product.create({ ...productData, image: imagesUrl });
 
-        res.json({success: true, message: "Product Added"})
-
-    } catch (error) {
-        console.log(error.message);
-        res.json({success: false, message: error.message})
-    }
-}
+    res.json({ success: true, message: "Product Added" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Get Product
-export const Productlist = async (req, res) => {
-    
-}
+export const productList = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Get single Product
 export const productById = async (req, res) => {
-    
-}
+  try {
+    const { id } = req.body;
+    const product = await Product.findById(id);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Change product inStock
 export const changeStock = async (req, res) => {
-    
-}
+  try {
+    const { id, inStock } = req.body;
+    await Product.findByIdAndUpdate(id, { inStock });
+    res.json({ success: true, message: "Stock Update" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
