58b392e2564c5c52c669a5549958490867cdcd37
omrajkamat68 <ob2614@srmist.edu.in>
2025-04-16 10:39:46 +0530

    userController updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -72,8 +72,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -105,8 +105,8 @@ export const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
     });
     return res.json({ success: true, message: "Logged Out" });
   } catch (error) {
